c2967649114e876786620902b4901ac354293221
Ibrahim-Hesham <ibrahim.hesham.cs@gmail.com>
2021-06-21 23:26:21 +0200

    .

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   ServerIP: `http://192.168.1.42:5000`,
-=======
-  ServerIP: `http://30.30.30.113:5000`,
->>>>>>> 8ba97f7102266c59b4358cd8466cdc15238c2992
   stackBackgroundColor: "#007cff",
 };
